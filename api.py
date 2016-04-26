1b68353ee9f514ecdb75e0fe22400c242c28ec57
arrancardnell <arrancardnell@gmail.com>
2016-04-26 12:48:59 +0100

    Added Giantbomb API class

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-import requests
+import requests, logging
 
 CPI_DATA_URL = 'http://research.stlouisfed.org/fred2/data/CPIAUCSL.txt'
 
@@ -89,6 +89,88 @@ class CPIData(object):
         current_cpi = self.year_cpi[current_year]
 
         return float(price) / year_cpi * current_cpi
+
+def is_valid_dataset(platform):
+    """Filters out datasets that can't be used since they are either lacking
+    a release date or an original price. For rendering the output we also
+    require the name and abbreviation of the platform.
+
+    """
+    
+    if 'release_date' not in platform or not platform['release_date']:
+        logging.warn(u"{0} has no release date".format(platform['name']))
+        return False
+    if 'original_price' not in platform or not platform['original_price']:
+        logging.warn(u"{0} has no original price".format(platform['name']))
+        return False
+    if 'name' not in platform or not platform['name']:
+        logging.warn(u"No platform name found or given dataset")
+        return False
+    if 'abbreviation' not in platform or not platform['abbreviation']:
+        logging.warn(u"{0} has no abbreviation".format(platform['name']))
+        return False
+    return True
+
+
+class GiantbombAPI(object):
+    """
+    Very simple implementation of the Giantbomb API that only offers the
+    GET /platforms/ call as a generator.
+
+    """
+
+    base_url = 'http://www.giantbomb.com/api'
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_platforms(self, sort=None, filter=None, field_list=None):
+        """Generator yielding platforms matching the given criteria. If n
+        limit is specified, this will return *all* platforms.
+
+        """
+
+        params = {}
+        if sort is not None:
+            params['sort'] = sort
+        if field_list is not None:
+            params['field_list'] = ','.join(field_list)
+        if filter is not None:
+            params['filter'] = filter
+            parsed_filters = []
+            for key, value in filter.iteritems():
+                parsed_filters.append('{0}:{1}'.format(key, value))
+            params['filters'] = ','.join(parsed_filters)
+
+        params['api_key'] = self.api_key
+        params['format'] = 'json'
+
+        incomplete_result = True
+        num_total_results = None
+        num_fetched_results = 0
+        counter = 0
+
+        while incomplete_result:
+            params['offset'] = num_fetched_results
+            result = requests.get(self.base_url + '/platforms/',
+                                  params=params)
+            result = result.json()
+            if num_total_results is None:
+                num_total_results = int(result['number_of_total_results'])
+            num_fetched_results += int(result['number_of_page_results'])
+            if num_fetched_results >= num_total_results:
+                incomplete_result = False
+            for item in result['results']:
+                logging.debug("Yielding platform {0} of {1}".format(
+                    counter + 1,
+                    num_total_results
+                ))
+
+                if 'original_price' in item and item['original_price']:
+                    item['original_price'] = float(item['original_price'])
+
+                yield item
+                counter += 1
 
 def main():
     """This function handles the actual logic of this script."""
